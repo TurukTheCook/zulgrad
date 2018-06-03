ce50c7faee92e848dc82e1534b85cd736b82d53d
TurukTheCook <yoann.roule@gmail.com>
2018-06-03 22:55:14 +0200

    feat: module add in back-end operationnal, still need to serve it and get the request. ALMOST DONE !

--- frontend/src/components/home/modulesManagement/Add.vue
+++ frontend/src/components/home/modulesManagement/Add.vue
@@ -7,11 +7,11 @@
         <div class="text-center">
           <small>All fields are required</small>
         </div>
-        <div class="form-group mb-2">
+        <div class="form-group mb-3">
           <label for="inputName">Name</label>
           <input v-model="newModule.name" type="text" id="inputName" class="form-control" required autofocus>
         </div>
-        <div class="form-group mb-2">
+        <div class="form-group mb-3">
           <label for="inputLabel">Label</label>
           <select v-model="newModule.label" class="form-control" id="inputLabel" required>
             <option value="source">Source</option>
@@ -20,7 +20,7 @@
           <small v-if="newModule.label == 'source'" class="text-primary">Source calls for only one source</small>
           <small v-if="newModule.label == 'countcat'" class="text-primary">You have to select one country and one category</small>
         </div>
-        <div v-if="newModule.label == 'source'" class="form-group mb-2">
+        <div v-if="newModule.label == 'source'" class="form-group mb-3">
           <label for="inputSource">Source</label>
           <input v-model="newModule.args.source" type="text" id="inputSource" class="form-control" :placeholder="sourceValue" disabled>
           <small class="text-primary">Select one in the list below</small><br>
@@ -29,7 +29,7 @@
             <div class="sources-wrapper d-flex flex-wrap justify-content-center">
               <div class="w-100 m-1">
                 <div class="form-group">
-                  <label for="inputSearch">Search <small class="text-primary">(by label, name, language [e.g.:  en], country [e.g.:  us], category)</small></label>
+                  <label for="inputSearch"><strong class="pl-3">Search</strong> <small class="text-primary">(by label, name, language [e.g.:  en], country [e.g.:  us], category)</small></label>
                   <input v-model="search" type="text" id="inputSearch" class="form-control">
                 </div>
               </div>
@@ -49,7 +49,7 @@
             </div>
           </div>
         </div>
-        <div v-if="newModule.label == 'countcat'" class="form-group mb-2">
+        <div v-if="newModule.label == 'countcat'" class="form-group mb-3">
           <label for="inputCountry">Country</label>
           <input v-model="newModule.args.country" type="text" id="inputCountry" class="form-control" :placeholder="countryValue" disabled>
           <small class="text-primary">Select one in the list below</small><br>
@@ -64,7 +64,7 @@
             </div>
           </div>
         </div>
-        <div v-if="newModule.label == 'countcat'" class="form-group mb-2">
+        <div v-if="newModule.label == 'countcat'" class="form-group mb-3">
           <label for="inputCategory">Category</label>
           <select v-model="newModule.args.category" class="form-control">
             <option value="business">Business</option>
@@ -76,8 +76,9 @@
             <option value="technology">Technology</option>
           </select>
         </div>
-        <div v-if="!success" class="alert alert-warning m-0 mb-2 w-100">{{message}}</div>
-        <button class="btn btn-lg btn-outline-primary btn-block mt-3 mb-2" type="submit" :disabled="calling">Send</button>
+        <div v-if="success == false" class="alert alert-warning m-0 mb-2 w-100">{{message}}</div>
+        <div v-if="success == true" class="alert alert-success m-0 mb-2 w-100">{{message}}</div>
+        <button class="btn btn-lg btn-outline-primary btn-block mt-5 mb-2" type="submit" :disabled="calling">Send</button>
       </form>
     </div>
   </div>
@@ -90,7 +91,7 @@ export default {
   name: 'moduleAdd',
   data() {
     return {
-      success: true,
+      success: null,
       message: null,
       calling: false,
       loading: true,
@@ -169,15 +170,39 @@ export default {
   },
   methods: {
     addModule() {
-      // todo
+      this.calling = true
+      let data = {
+        module: {
+          args: {}
+        }
+      }
+      if (this.newModule.label == 'source') {
+        data.module.label = 'source'
+        data.module.args.source = this.newModule.args.source
+      } else {
+        data.module.args.country = this.newModule.args.country
+        data.module.args.category = this.newModule.args.category
+      }
+      data.module.name = this.newModule.name
+      http.post('modules', data)
+        .then(res => {
+          this.calling = false
+          this.success = res.data.success
+          this.message = res.data.message
+        })
+        .catch(err => {
+          this.calling = false
+          this.success = err.response.data.success
+          this.message = err.response.data.message
+        })
     },
     selectSource(source) {
-      this.newModule.args.source = source.label;
-      this.sourceValue = source.label;
+      this.newModule.args.source = source.label
+      this.sourceValue = source.label
     },
     selectCountry(country) {
-      this.newModule.args.country = country.code;
-      this.countryValue = country.name;
+      this.newModule.args.country = country.code
+      this.countryValue = country.name
     },
     fetchData() {
       http.get('sources')
