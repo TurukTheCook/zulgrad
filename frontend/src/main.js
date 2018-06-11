acda91cba4e3e67653d175821a5cc2fff79c06c4
TurukTheCook <yoann.roule@gmail.com>
2018-06-11 19:32:46 +0200

    little change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import { SSL_OP_COOKIE_EXCHANGE } from 'constants'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { SSL_OP_COOKIE_EXCHANGE } from 'constants'
 import store from './store'
 
 /**
